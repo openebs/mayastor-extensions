f4cae493c3d669535231655ad3cff1210cd1941b
Tiago Castro <tiagolobocastro@gmail.com>
2023-01-16 15:41:13 +0000

    fix(gha/bot): update README when creating develop=>release PR (#97)
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- scripts/helm/generate-readme.sh
+++ scripts/helm/generate-readme.sh
@@ -4,10 +4,12 @@ SCRIPTDIR=$(dirname "$0")
 ROOTDIR="$SCRIPTDIR"/../../
 TEMPLATE="$ROOTDIR/chart/README.md.tmpl"
 README="$ROOTDIR/chart/README.md"
+SKIP_GIT=${SKIP_GIT:-}
 
 set -euo pipefail
 
 helm-docs --dry-run -t "$TEMPLATE" > "$README"
 
-git diff --exit-code "$README"
-
+if [ -z "$SKIP_GIT" ]; then
+  git diff --exit-code "$README"
+fi
