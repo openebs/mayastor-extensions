1aa0d98523b3eb6a23a18939712461cdee9af000
sinhaashish <ashi.sinha.87@gmail.com>
2023-03-30 11:05:19 +0000

    feat(upgrade): complete the dry run flag
    Signed-off-by: sinhaashish <ashi.sinha.87@gmail.com>

--- console-logger/src/console.rs
+++ console-logger/src/console.rs
@@ -1,8 +1,12 @@
 use nu_ansi_term::Color::{Cyan, Red};
 
 /// Print info on console.
-pub fn info(message: &str) {
-    println!("{}", Cyan.bold().italic().paint(message));
+pub fn info(message: &str, data: &str) {
+    println!(
+        "{} \n {} ",
+        Cyan.bold().italic().paint(message),
+        Cyan.bold().italic().paint(data)
+    );
 }
 
 /// Print warning on console.
--- k8s/upgrade/src/lib.rs
+++ k8s/upgrade/src/lib.rs
@@ -1,10 +1,10 @@
-/// Module for upgrade related constants
+/// Module for upgrade related constants.
 pub mod constant;
-/// Validations before applying upgrade
+/// Validations before applying upgrade.
 pub mod preflight_validations;
 /// Library for upgrade module.
 pub mod upgrade_lib;
-/// Module for resources of upgrade
+/// Module for resources of upgrade.
 pub mod upgrade_resources;
-/// Module for user messages
+/// Module for user messages.
 pub mod user_prompt;
