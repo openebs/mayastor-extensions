a50d7de02b81ac536a732c296ad85e616168886c
sinhaashish <ashi.sinha.87@gmail.com>
2023-05-17 07:56:34 +0000

    feat(upgrade): update upgrade-job-image-tag
    Signed-off-by: sinhaashish <ashi.sinha.87@gmail.com>

--- k8s/upgrade/src/constant.rs
+++ k8s/upgrade/src/constant.rs
@@ -67,7 +67,7 @@ pub(crate) const HELM_RELEASE_NAME_LABEL: &str = "openebs.io/release";
 
 pub(crate) const DEFAULT_IMAGE_REGISTRY: &str = "docker.io";
 /// The upgrade job will use the UPGRADE_JOB_IMAGE_NAME image (below) with this tag.
-pub(crate) const UPGRADE_JOB_IMAGE_TAG: &str = "develop";
+pub(crate) const UPGRADE_JOB_IMAGE_TAG: &str = "release-2.2";
 /// Upgrade job container image repository.
 pub(crate) const UPGRADE_JOB_IMAGE_REPO: &str = "openebs";
 /// Upgrade job container image name.
