4e4faefc557b7fd02514770fe6951d2eaba1aa70
Tiago Castro <tiagolobocastro@gmail.com>
2023-10-27 17:36:14 +0100

    fix(kubectl-plugin): dump using non-default namespace
    Namespace was not being taken from cli args when creating the rest client.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- k8s/supportability/src/collect/utils.rs
+++ k8s/supportability/src/collect/utils.rs
@@ -1,10 +1,10 @@
 use once_cell::sync::OnceCell;
 use std::{fs::File, io::Write, path::PathBuf};
 
-/// TOOL LOG FILE is the file that stores the logs of the support tool
-static TOOL_LOG_FILE: OnceCell<File> = OnceCell::new();
+/// TOOL LOG FILE is the file that stores the logs of the support tool.
+static TOOL_LOG_FILE: OnceCell<Option<File>> = OnceCell::new();
 
-/// Method to be only used to print tool logs to console and write in file
+/// Method to be only used to print tool logs to console and write in file.
 pub fn log(content: String) {
     println!("{content}");
     // NOTE: If we failed to write to log file can't do anything, just write
@@ -13,28 +13,47 @@ pub fn log(content: String) {
         .map_err(|e| println!("Not be able to write to log file, error: {e}"));
 }
 
-/// Method to be only used to write in file
+/// Method to be only used to write in file.
 pub(crate) fn write_to_log_file(content: String) -> Result<(), std::io::Error> {
-    TOOL_LOG_FILE
+    if let Some(mut file) = TOOL_LOG_FILE
         .get()
-        .expect("TOOL_LOG_FILE should have been initialised")
-        .write_all(content.as_bytes())?;
+        .ok_or(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "LogFile not initialised!",
+        ))?
+        .as_ref()
+    {
+        file.write_all(content.as_bytes())?;
+    }
+
     Ok(())
 }
 
-/// Method to initialise the TOOL_LOG_FILE once cell with a File
+/// Method to initialise the TOOL_LOG_FILE once cell with a File.
 pub(crate) fn init_tool_log_file(file_path: PathBuf) -> Result<(), std::io::Error> {
     TOOL_LOG_FILE
-        .set(File::create(file_path)?)
+        .set(Some(File::create(file_path)?))
         .expect("Expect to be initialised only once");
     Ok(())
 }
-
-/// Flush the stream
-pub fn flush_tool_log_file() -> Result<(), std::io::Error> {
+/// Method to initialise the TOOL_LOG_FILE once cell without a log file.
+pub(crate) fn init_no_log_file() {
     TOOL_LOG_FILE
+        .set(None)
+        .expect("Expect to be initialised only once");
+}
+
+/// Flush the stream.
+pub fn flush_tool_log_file() -> Result<(), std::io::Error> {
+    if let Some(mut file) = TOOL_LOG_FILE
         .get()
-        .expect("TOOL_LOG_FILE should have been initialised")
-        .flush()?;
+        .ok_or(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "LogFile not initialised!",
+        ))?
+        .as_ref()
+    {
+        file.flush()?;
+    }
     Ok(())
 }
