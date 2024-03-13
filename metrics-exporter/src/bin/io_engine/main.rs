79d5a44cccaa4458ebe9ce31a1487d71c8332353
Abhilash Shetty <abhilash.shetty@datacore.com>
2024-03-13 07:42:23 +0000

    feat(ansi_color): arg changes
    Signed-off-by: Abhilash Shetty <abhilash.shetty@datacore.com>

--- metrics-exporter/src/bin/io_engine/main.rs
+++ metrics-exporter/src/bin/io_engine/main.rs
@@ -48,7 +48,7 @@ pub(crate) struct Cli {
     fmt_style: FmtStyle,
 
     /// Use ANSI colors for the logs.
-    #[clap(long)]
+    #[clap(long, default_value_t = true, action = clap::ArgAction::Set)]
     ansi_colors: bool,
 }
 
