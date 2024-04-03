149f494656ace953dc9fcafd2e8a6258f0222e9d
Niladri Halder <niladri.halder26@gmail.com>
2024-04-03 15:28:17 +0000

    feat(scripts/publish-chart): change dependency crds chart version when generating release chart
    Signed-off-by: Niladri Halder <niladri.halder26@gmail.com>

--- scripts/helm/publish-chart-yaml.sh
+++ scripts/helm/publish-chart-yaml.sh
@@ -374,6 +374,8 @@ if [ -n "$CHECK_BRANCH" ]; then
       sed -i "s/^appVersion:.*$/appVersion: \"$newChartAppVersion\"/" "$CHART_FILE"
       # Set same versions on the CRD subchart
       sed -i "s/^version:.*/version: $newChartVersion/" "$CRDS_SUBCHART_CHART_FILE"
+      yq_ibl "(.dependencies[] | select(.name == \"crds\").version) |= \"$newChartVersion\"" "$CHART_FILE"
+
       # Set image tag to empty because main branch uses repoTags.
       yq_ibl '.image.tag |= ""' "$CHART_VALUES"
       # always pull since image changes with commits to the branch
@@ -393,6 +395,8 @@ if [ -n "$CHECK_BRANCH" ]; then
         sed -i "s/^appVersion:.*$/appVersion: \"$newChartAppVersion\"/" "$CHART_FILE"
         # Set same versions on the CRD subchart
         sed -i "s/^version:.*/version: $newChartVersion/" "$CRDS_SUBCHART_CHART_FILE"
+        yq_ibl "(.dependencies[] | select(.name == \"crds\").version) |= \"$newChartVersion\"" "$CHART_FILE"
+
         # point image tag to the release branch images
         yq_ibl ".image.tag |= \"${CHECK_BRANCH////-}\"" "$CHART_VALUES"
         # always pull since image changes with commits to the branch
@@ -480,6 +484,8 @@ if [ -z "$DRY_RUN" ]; then
   sed -i "s/^appVersion:.*$/appVersion: \"$newChartAppVersion\"/" "$CHART_FILE"
   # Set same versions on the CRD subchart
   sed -i "s/^version:.*/version: $newChartVersion/" "$CRDS_SUBCHART_CHART_FILE"
+  yq_ibl "(.dependencies[] | select(.name == \"crds\").version) |= \"$newChartVersion\"" "$CHART_FILE"
+
   yq_ibl ".image.tag |= \"v$newChartAppVersion\"" "$CHART_VALUES"
   # tags are stable so we don't need to pull always
   yq_ibl ".image.pullPolicy |= \"IfNotPresent\"" "$CHART_VALUES"
