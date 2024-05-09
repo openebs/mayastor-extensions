889afe0689fe710245f0c95a99eccdd6bbb7779a
Niladri Halder <niladri.halder26@gmail.com>
2024-05-09 19:19:15 +0000

    feat(call-home): use pagination when listing k8s nodes
    Signed-off-by: Niladri Halder <niladri.halder26@gmail.com>

--- call-home/src/bin/callhome/collector/k8s_client.rs
+++ call-home/src/bin/callhome/collector/k8s_client.rs
@@ -1,6 +1,11 @@
 use k8s_openapi::api::core::v1::{Namespace, Node};
-use kube::{Api, Client};
+use kube::{
+    api::{Api, ListParams},
+    Client,
+};
 use obs::common::errors::K8sResourceError;
+
+const KUBE_API_PAGE_SIZE: u32 = 500;
 
 /// K8sClient contains k8s client.
 #[derive(Clone)]
@@ -17,15 +22,33 @@ impl K8sClient {
 
     /// Get number of nodes present in the cluster.
     pub(crate) async fn get_node_len(&self) -> Result<usize, K8sResourceError> {
+        let mut nodes_count: usize = 0;
+
         let nodes: Api<Node> = Api::all(self.client.clone());
-        let list = nodes.list(&Default::default()).await?;
-        Ok(list.items.len())
+        let mut list_params = ListParams::default().limit(KUBE_API_PAGE_SIZE);
+
+        loop {
+            let node_list = nodes.list_metadata(&list_params).await?;
+
+            let continue_ = node_list.metadata.continue_.clone();
+
+            nodes_count += node_list.items.len();
+
+            match continue_ {
+                Some(token) => {
+                    list_params = list_params.continue_token(token.as_str());
+                }
+                None => break,
+            }
+        }
+
+        Ok(nodes_count)
     }
 
     /// Get kube-system namespace uid.
     pub(crate) async fn get_cluster_id(&self) -> Result<String, K8sResourceError> {
         let namespace_api: Api<Namespace> = Api::all(self.client.clone());
-        let kube_system_namespace = namespace_api.get("kube-system").await?;
+        let kube_system_namespace = namespace_api.get_metadata("kube-system").await?;
         Ok(kube_system_namespace.metadata.uid.unwrap_or_default())
     }
 }
