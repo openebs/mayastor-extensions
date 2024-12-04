b8d72648dcd6dbd3d1cd46d8dd914a9bb111ba9b
Tiago Castro <tiagolobocastro@gmail.com>
2024-12-04 10:37:50 +0000

    build: update control-plane dep
    Also updates style script to match control-plane.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- scripts/rust/linter.sh
+++ scripts/rust/linter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cargo fmt --version
-cargo fmt --all
+cargo fmt --all -- --config imports_granularity=Crate
 
 cargo clippy --version
 cargo clippy --all --all-targets $1 -- -D warnings
