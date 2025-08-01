5445a186db4d9e341b13c436d5341e0713ab888a
Tiago Castro <tiagolobocastro@gmail.com>
2025-08-01 21:50:38 +0100

    build: update rust to 1.88.0 and nixpkgs to 25.05
    An unfortunate side effect of the nixpkgs update seems to be a slower
    entry in the nix-shell, not quite yet explained as of now...
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- nix/lib/rust.nix
+++ nix/lib/rust.nix
@@ -6,8 +6,8 @@ in
 rec {
   makeRustTarget = platform: pkgs.rust.toRustTargetSpec platform;
   rust_default = { override ? { } }: rec {
-    nightly_pkg = pkgs.rust-bin.nightly."2024-10-30";
-    stable_pkg = pkgs.rust-bin.stable."1.82.0";
+    nightly_pkg = pkgs.rust-bin.nightly."2025-06-26";
+    stable_pkg = pkgs.rust-bin.stable."1.88.0";
 
     nightly = nightly_pkg.default.override (override);
     stable = stable_pkg.default.override (override);
