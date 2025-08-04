a08149951e6da28d0af94d0009806a934aefadbb
Tiago Castro <tiagolobocastro@gmail.com>
2025-08-04 15:26:54 +0100

    refactor: use new rust target platform fn
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- nix/pkgs/extensions/cargo-project.nix
+++ nix/pkgs/extensions/cargo-project.nix
@@ -34,7 +34,7 @@ let
     rustc = channel.default.stable;
     cargo = channel.default.stable;
   };
-  static-target = pkgs.rust.toRustTargetSpec pkgs.pkgsStatic.hostPlatform;
+  static-target = channel.makeRustTarget pkgs.pkgsStatic.hostPlatform;
   static-channel = channel.rust_default {
     override = { targets = [ "${static-target}" ]; };
   };
@@ -97,7 +97,7 @@ let
   static_ssl = (pkgs.pkgsStatic.openssl.override {
     static = true;
   });
-  hostTarget = pkgs.rust.toRustTargetSpec pkgs.hostPlatform;
+  hostTarget = channel.makeRustTarget pkgs.hostPlatform;
   buildProps = rec {
     name = "extensions-${version}";
     inherit version src;
