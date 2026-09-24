0c875364336495cca2d265ab3b774ef20a0ad26a
Tiago Castro <tiagolobocastro@gmail.com>
2026-09-24 17:05:48 +0100

    chore: ensure tmp is the same when under vscode
    This ensures plugins which don't use a fixed TMP work properly
    We may also ask the question, should we do this for everything and not just
    for vscode?
    I'll leave that for another discussion and for now settle on just vscode
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- shell.nix
+++ shell.nix
@@ -73,6 +73,13 @@ pkgs.mkShellNoCC {
       pre-commit install
       pre-commit install --hook commit-msg
     fi
+    # Looks like vscode with the nix plugin is running commands in different shells without setting the temp variables
+    # Since nix-shell by default nests the temp variables, we need to set them here
+    if [ "$VSCODE_CLI" = "1" ]; then
+      export TMPDIR=/tmp
+      export TMP=/tmp
+    fi
+
     export EXTENSIONS_SRC=`pwd`
     export CTRL_SRC="$EXTENSIONS_SRC"/dependencies/control-plane
     export PATH="$(pwd)/target/debug:$PATH"
